32400dcb8e184dee49db5d2baf4f29fc4dc2cc04
Alex Zadorozhin <zadorozhin.a@wb.ru>
2024-07-30 14:19:11 +0700

    Hosts, shards, C4

--- database/_travel_network.sql
+++ database/_travel_network.sql
@@ -2,7 +2,8 @@ CREATE TABLE "subscriptions" (
   "follower_user_id" integer,
   "followee_user_id" integer,
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'follower_user_id', colocate_with = 'users', num_parts = 4);
 
 CREATE TABLE "users" (
   "id" integer PRIMARY KEY,
@@ -10,7 +11,8 @@ CREATE TABLE "users" (
   "email" varchar,
   "password" varchar,
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'id', num_parts = 4);
 
 CREATE TABLE "posts" (
   "id" integer PRIMARY KEY,
@@ -21,31 +23,35 @@ CREATE TABLE "posts" (
   "place" integer,
   "photos" integer[],
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'user_id', colocate_with = 'users', num_parts = 4);
 
 CREATE TABLE "places" (
   "id" integer PRIMARY KEY,
   "name" varchar,
   "country" varchar,
   "city" varchar,
-  "latitude" double,
-  "longitude" double,
+  "latitude" double precision,
+  "longitude" double precision,
   "created_at" timestamp
-);
+)
+    WITH (global);
 
 CREATE TABLE "photos" (
   "id" integer PRIMARY KEY,
   "url" varchar,
   "user_id" integer,
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'user_id', colocate_with = 'users', num_parts = 4);
 
 CREATE TABLE "messages" (
   "id" integer PRIMARY KEY,
   "sender_user_id" integer,
   "receiver_user_id" integer,
   "created_at" timestamp
-);
+)
+    WITH (global);
 
 CREATE TABLE "rating" (
   "id" integer PRIMARY KEY,
@@ -53,14 +59,16 @@ CREATE TABLE "rating" (
   "user_id" integer,
   "value" integer,
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'post_id', colocate_with = 'posts', num_parts = 4);
 
 CREATE TABLE "rate_history" (
   "id" integer PRIMARY KEY,
   "post_id" integer,
   "value" integer,
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'post_id', colocate_with = 'posts', num_parts = 4);
 
 CREATE TABLE "comments" (
   "id" integer PRIMARY KEY,
@@ -68,7 +76,8 @@ CREATE TABLE "comments" (
   "user_id" integer,
   "text" varchar,
   "created_at" timestamp
-);
+)
+WITH (distributed_by = 'post_id', colocate_with = 'posts', num_parts = 4);
 
 COMMENT ON COLUMN "posts"."description" IS 'Content of the post';
 
